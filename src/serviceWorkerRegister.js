95a197003e5edccddebc10d91e504902cf832e26
Lucija Klanjac <77996294+lus-terry@users.noreply.github.com>
2025-01-31 02:03:21 +0100

    Update serviceWorkerRegister.js

--- src/serviceWorkerRegister.js
+++ src/serviceWorkerRegister.js
@@ -1,14 +1,22 @@
 export function registerServiceWorker() {
+
     if ('serviceWorker' in navigator && 'SyncManager' in window) {
-      navigator.serviceWorker.register('/sw.js').then((registration) => {
-        console.log('Service Worker registriran:', registration);
-        
-        navigator.serviceWorker.ready.then((swRegistration) => {
-          return swRegistration.sync.register('sync-data');
+      navigator.serviceWorker
+        .register('/sw.js') 
+        .then((registration) => {
+          console.log('Service Worker registriran:', registration);
+  
+          navigator.serviceWorker.ready.then((swRegistration) => {
+            return swRegistration.sync.register('sync-data').then(() => {
+              console.log('Background Sync registriran s oznakom: sync-data');
+            });
+          });
+        })
+        .catch((error) => {
+          console.error('Greška pri registraciji SW:', error);
         });
-      }).catch((error) => {
-        console.error('Greška pri registraciji SW:', error);
-      });
+    } else {
+      console.warn('Service Worker ili Background Sync nisu podržani u ovom pregledniku.');
     }
   }
   
